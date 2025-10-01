41fec0631c0994dbadc404d07c24764593631b85
Skyspec <admin@skyspec.com.au>
2025-10-01 21:33:44 +1000

    Update __init__.py

--- scraper/sources/__init__.py
+++ scraper/sources/__init__.py
@@ -5,3 +5,10 @@ AVAILABLE_SOURCES = {
     "nsw_major_projects": NSWMajorProjects,
     "qld_qtenders": QLDQTenders,
 }
+from .icn_gateway import ICNGateway
+from .rss_feeds import RSSFeeds
+
+AVAILABLE_SOURCES.update({
+    "icn_gateway": ICNGateway,
+    "rss_feeds": RSSFeeds,
+})
